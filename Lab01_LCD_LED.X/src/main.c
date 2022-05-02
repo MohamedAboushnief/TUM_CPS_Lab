1676ef0158e9e5b24c105a70c0034b42a03541d5
MohamedAboushnief <mohamed.shenif@gmail.com>
2022-05-02 21:42:08 +0200

    Correct implementation

--- Lab01_LCD_LED.X/src/main.c
+++ Lab01_LCD_LED.X/src/main.c
@@ -19,61 +19,56 @@
 #pragma config FWDTEN = OFF
 
 
-void turnLedOn(int counter);
+void ledBinary(int binaryValueArr[]);
 void delay(long int count);
-void clearAllLeds(void);
+void ledOutputInit(void);
+void decToBinary(int n);
 
 
 
-void turnLedOn(int counter) {
+void ledBinary(int binaryValueArr[]) {
     //do the LED part
-    switch (counter) {
-        case 1:
-            SETLED(LED1_PORT);
-            Nop();
-            lcd_printf("1");
-            lcd_locate(10, 5);
-            break;
-        case 2:
-            SETLED(LED2_PORT);
-            Nop();
-            lcd_printf("2");
-            lcd_locate(10, 5);
-            break;
-        case 3:
-            SETLED(LED3_PORT);
-            Nop();
-            lcd_printf("3");
-            lcd_locate(10, 5);
-            break;
-        case 4:
-            SETLED(LED4_PORT);
-            Nop();
-            lcd_printf("4");
-            lcd_locate(10, 5);
-            break;
-        case 5:
-            SETLED(LED5_PORT);
-            Nop();
-            lcd_printf("5");
-            lcd_locate(10, 5);
-            break;
-        case 6:
-            CLEARLED(LED1_PORT);
-            Nop();
-            CLEARLED(LED2_PORT);
-            Nop();
-            CLEARLED(LED3_PORT);
-            Nop();
-            CLEARLED(LED4_PORT);
-            Nop();
-            CLEARLED(LED5_PORT);
-            break;
+
+    if (binaryValueArr[0] == 0) {
+        CLEARLED(LED5_PORT);
+        Nop();
+    } else {
+        SETLED(LED5_PORT);
+        Nop();
     }
+    if (binaryValueArr[1] == 0) {
+        CLEARLED(LED4_PORT);
+        Nop();
+    } else {
+        SETLED(LED4_PORT);
+        Nop();
+    }
+    if (binaryValueArr[2] == 0) {
+        CLEARLED(LED3_PORT);
+        Nop();
+    } else {
+        SETLED(LED3_PORT);
+        Nop();
+    }
+    if (binaryValueArr[3] == 0) {
+        CLEARLED(LED2_PORT);
+        Nop();
+    } else {
+        SETLED(LED2_PORT);
+        Nop();
+    }
+    if (binaryValueArr[4] == 0) {
+        CLEARLED(LED1_PORT);
+        Nop();
+    } else {
+        SETLED(LED1_PORT);
+        Nop();
+    }
+
+
 }
 
-
-void clearAllLeds(void){
+void ledOutputInit(void) {
     CLEARLED(LED1_TRIS);
     Nop();
     CLEARLED(LED2_TRIS);
@@ -85,89 +80,87 @@ void clearAllLeds(void){
     CLEARLED(LED5_TRIS);
     Nop();
 }
-//
-//
-//void decToBinary(int n)
-//{
-//    // array to store binary number
-//    int binaryNum[32];
-// 
-//    // counter for binary array
-//    int i = 0;
-//    while (n > 0) {
-// 
-//        // storing remainder in binary array
-//        binaryNum[i] = n % 2;
-//        n = n / 2;
-//        i++;
-//    }
-// 
-//    // printing binary array in reverse order
-//    for (int j = i - 1; j >= 0; j--){
-//        lcd_locate(10, 5);
-//        lcd_printf("%c",binaryNum[j]);
-////        cout << binaryNum[j];
-//    }
-//}
+
+void decToBinary(int n) {
+    // array to store binary number
+    int binaryNum[5] = {0, 0, 0, 0, 0};
+
+    // counter for binary array
+    int i = 0;
+    while (n > 0) {
+
+        // storing remainder in binary array
+        binaryNum[i] = n % 2;
+        n = n / 2;
+        i++;
+    }
 
 
-void delay(long int count){
-    long int i=0;
-    while(i<count){
+    ledBinary(binaryNum);
+}
+
+void delay(long int count) {
+    long int i = 0;
+    while (i < count) {
         i++;
     }
 }
 
+int main() {
+    //Init LCD and LEDs
+    lcd_initialize();
+    led_init();
 
-    int main() {
-        //Init LCD and LEDs
-        lcd_initialize();
-        led_init();
-
-        // Clear the Screen and reset the cursor
-        lcd_clear();
-        lcd_locate(0, 0);
-
-
-        // Print Hello World
-        lcd_printf("Mohamed Aboushenif");
-        lcd_locate(0, 1);
-        lcd_printf("Xinlan Zheng\n");
-        lcd_locate(0, 2);
-        lcd_printf("Qiu Rui");
-
-        
-
-        clearAllLeds();
+    // Clear the Screen and reset the cursor
+    lcd_clear();
+    lcd_locate(0, 0);
 
 
-        lcd_locate(0, 5);
-        lcd_printf("Counter:");
+    lcd_printf("Mohamed Aboushenif");
+    lcd_locate(0, 1);
+    lcd_printf("Xinlan Zheng\n");
+    lcd_locate(0, 2);
+    lcd_printf("Qiu Rui");
+
+
+
+    ledOutputInit();
+
+
+    lcd_locate(0, 5);
+    lcd_printf("Counter:");
+    lcd_locate(10, 5);
+
+
+
+
+    int counter = 0;
+    while (1) {
+
+
+
+        decToBinary(counter);
+
+        lcd_printf("%d", counter);
         lcd_locate(10, 5);
 
-        int counter = 1;
-        while (1) {
+        // maximum number in 5 binary digits is 31
+        if (counter == 32) {
+            counter = 0;
+            lcd_printf("  ");
+            lcd_locate(10, 5);
 
-            
-            
-            turnLedOn(counter);
-
-
-            if (counter == 6) {
-                counter = 1;
-
-            }
-            else{
-                counter++;
-            }
-
-            
-            delay(1000000);
-            
-            
-            
-
+        } else {
+            counter++;
         }
 
+
+        delay(1000000);
+
+
+
+
     }
+
+}
 
