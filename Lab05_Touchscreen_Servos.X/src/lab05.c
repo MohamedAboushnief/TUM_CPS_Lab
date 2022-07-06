4b91964423456b9b6429383f1b09cd1edef7053e
Mohamed Aboushenif <mohamed.shenif@gmail.com>
2022-07-06 12:38:48 +0200

    Continue Task 5

--- Lab05_Touchscreen_Servos.X/src/lab05.c
+++ Lab05_Touchscreen_Servos.X/src/lab05.c
@@ -15,6 +15,12 @@ typedef enum{
     SERVO_2
 }SERVO;
 
+typedef enum{
+    x,
+    y
+    
+}DIMENSION;
+
 
 /*
  * PWM code
@@ -30,9 +36,94 @@ typedef enum{
 #define PWM_MAX_US 2000
 #define PWM_CYC_US 20000
 
+
+void adc_init(void)
+{
+    //disable ADC
+    CLEARBIT(AD1CON1bits.ADON);
+    
+    //initialize PINS
+    SETBIT(TRISBbits.TRISB15); //set TRISE RB15 to input
+    CLEARBIT(AD1PCFGLbits.PCFG15); //set AD1 AN15 input pin as analog
+    
+    SETBIT(TRISBbits.TRISB9); //set TRISE RB9  to input
+    CLEARBIT(AD1PCFGLbits.PCFG9); //set AD1 AN9 input pin as analog
+
+    //Configure AD1CON1
+    CLEARBIT(AD1CON1bits.AD12B) //set 10b Operation Mode
+    AD1CON1bits.FORM = 0; //set integer output
+    AD1CON1bits.SSRC = 0x7; //set automatic conversion
+    //Configure AD1CON2
+    AD1CON2 = 0; //not using scanning sampling
+    //Configure AD1CON3
+    CLEARBIT(AD1CON3bits.ADRC); //internal clock source
+    AD1CON3bits.SAMC = 0x1F; //sample-to-conversion clock = 31Tad
+    AD1CON3bits.ADCS = 0x2; //Tad = 3Tcy (Time cycles)
+    //Leave AD1CON4 at its default value
+    //enable ADC
+    SETBIT(AD1CON1bits.ADON);
+}
+
+    
+
+
 /*
  * touch screen code
  */
+void touch_screen_init(void){
+    
+    adc_init();
+    //set up the I/O pins E1, E2, E3 to be output pins
+    CLEARBIT(TRISEbits.TRISE1); //I/O pin set to output
+    CLEARBIT(TRISEbits.TRISE2); //I/O pin set to output
+    CLEARBIT(TRISEbits.TRISE3); //I/O pin set to output
+   
+    // x,y standby mode
+    
+    SETBIT(PORTEbits.RE1);
+    SETBIT(PORTEbits.RE2);
+    CLEARBIT(PORTEbits.RE3);
+    
+ 
+}
+
+touch_screen_dimension_set(DIMENSION dim){
+    
+    if(dim == x){
+        // set x coordinate as read
+        CLEARBIT(PORTEbits.RE1);
+        SETBIT(PORTEbits.RE2);
+        SETBIT(PORTEbits.RE3);
+    }
+    else if(dim == y){
+        // set y coordinate as read
+        SETBIT(PORTEbits.RE1);
+        CLEARBIT(PORTEbits.RE2);
+        CLEARBIT(PORTEbits.RE3);
+    }
+}
+
+
+uint16_t touch_screen_position_read_x(void)
+{
+    AD1CHS0bits.CH0SA = 0x0F; //set ADC to Sample AN20 pin
+    SETBIT(AD1CON1bits.SAMP); //start to sample
+    while(!AD1CON1bits.DONE); //wait for conversion to finish
+    CLEARBIT(AD1CON1bits.DONE); //MUST HAVE! clear conversion done bit
+    return ADC1BUF0; //return sample
+    
+}
+uint16_t touch_screen_position_read_y(void)
+{
+    AD1CHS0bits.CH0SA = 0x09; //set ADC to Sample AN20 pin
+    SETBIT(AD1CON1bits.SAMP); //start to sample
+    while(!AD1CON1bits.DONE); //wait for conversion to finish
+    CLEARBIT(AD1CON1bits.DONE); //MUST HAVE! clear conversion done bit
+    return ADC1BUF0; //return sample
+    
+}
+
+
 void pwm_init(SERVO servo_x ){
     
     
@@ -49,14 +140,15 @@ void pwm_init(SERVO servo_x ){
         PR2 = 4000; // Set timer period 20ms: // 4000= 20*10^-3 * 12.8*10^6 * 1/64
         //setup OC8
         CLEARBIT(TRISDbits.TRISD7); /* Set OC8 as output */
-        OC8R = 1000; /* Set the initial duty cycle to 5ms*/
-        OC8RS = 1000; /* Load OCRS: next pwm duty cycle */
+        OC8R = 3820; /* Set the initial duty cycle to 5ms*/
+        OC8RS = 3820; /* Load OCRS: next pwm duty cycle */
         OC8CON = 0x0006; /* Set OC8: PWM, no fault check, Timer2 */
         SETBIT(T2CONbits.TON); /* Turn Timer 2 on */
     }
     else if(servo_x==SERVO_2)
     {
             //setup Timer 2
+            //The following code sets up OC8 to work in PWM mode and be controlled by Timer 2.
         CLEARBIT(T2CONbits.TON); // Disable Timer
         CLEARBIT(T2CONbits.TCS); // Select internal instruction cycle clock
         CLEARBIT(T2CONbits.TGATE); // Disable Gated Timer mode
@@ -67,8 +159,8 @@ void pwm_init(SERVO servo_x ){
         PR2 = 4000; // Set timer period 20ms: // 4000= 20*10^-3 * 12.8*10^6 * 1/64
         //setup OC8
         CLEARBIT(TRISDbits.TRISD6); /* Set OC7 as output */
-        OC7R = 1000; /* Set the initial duty cycle to 5ms*/
-        OC7RS = 1000; /* Load OCRS: next pwm duty cycle */
+        OC7R = 3820; /* Set the initial duty cycle to 5ms*/
+        OC7RS = 3820; /* Load OCRS: next pwm duty cycle */
         OC7CON = 0x0006; /* Set OC8: PWM, no fault check, Timer2 */
         SETBIT(T2CONbits.TON); /* Turn Timer 2 on */
     }
@@ -82,11 +174,29 @@ void servo_init(SERVO servo_x)
 {
     if(servo_x==SERVO_1)
     {
-        
+        pwm_init(SERVO_1);
     }
     else if(servo_x==SERVO_2)
     {
-        
+        pwm_init(SERVO_2);
+    }
+    
+}
+
+
+void servo_duty_cycle_set(SERVO servo_x, float duty_cycle)
+{
+     if(servo_x==SERVO_1)
+    {
+         
+        OC8R = (duty_cycle/20.0)*4000; 
+        OC8RS = (duty_cycle/20.0)*4000; /* Load OCRS: next pwm duty cycle */
+
+    }
+    else if(servo_x==SERVO_2)
+    {
+        OC7R = (duty_cycle/20.0)*4000; 
+        OC7RS = (duty_cycle/20.0)*4000; /* Load OCRS: next pwm duty cycle */
     }
     
 }
@@ -106,8 +216,20 @@ void main_loop()
     // initialize touchscreen
     
     // initialize servos
+    servo_init(SERVO_1);
+    Nop();
+    servo_init(SERVO_2);
+    Nop();
     
-    while(TRUE) {
+    while(TRUE) { 
         
+        servo_duty_cycle_set(SERVO_1,20-2.1);
+        Nop();
+        
+        
+        servo_duty_cycle_set(SERVO_2,20-2.1);
+        Nop();
+
+
     }
 }
