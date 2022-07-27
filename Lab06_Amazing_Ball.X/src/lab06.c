de2886de8c457131e4d07dc329d307ee7ef12a14
Juyi9640 <xinlanair@gmail.com>
2022-07-27 11:52:03 +0200

    finish lab6

--- Lab06_Amazing_Ball.X/src/lab06.c
+++ Lab06_Amazing_Ball.X/src/lab06.c
@@ -12,7 +12,6 @@
 #include "lcd.h"
 #include "led.h"
 
-
 typedef enum {
     SERVO_1,
     SERVO_2
@@ -24,26 +23,17 @@ typedef enum {
 
 } DIMENSION;
 
-volatile int counterHundredHz = 0;
-volatile int counterFiftyHz = 0;
-volatile int counterFiveHz = 0;
 
-volatile bool trigger_read_touchscreen = false;
-volatile bool trigger_pd_controller = false;
-volatile bool trigger_print_missed_deadlines = false;
 /*
  * Parameter
  */
 #define pi 3.14159265358979323846
 #define center_x 409
-#define center_y 371
+#define center_y 310
 
-#define radius 224
-#define frequency 1
-#define angular_velocity 2*pi*frequency
-
-
-
+#define radius 200
+#define frequency 2.5
+#define angular_velocity_w 2*pi*frequency
 
 
 /*
@@ -54,14 +44,52 @@ volatile bool trigger_print_missed_deadlines = false;
 #define TCKPS_64  0x02
 #define TCKPS_256 0x03
 
-
-
 /*
  * Global Variables
  */
-uint16_t x_pos = 0;
-uint16_t y_pos = 0;
-bool trigger_exec = false;
+
+volatile int counterHundredHz = 0;
+volatile int counterFiftyHz = 0;
+volatile int counterFiveHz = 0;
+
+volatile bool trigger_read_touchscreen = false;
+volatile bool trigger_pd_controller = false;
+volatile bool trigger_print_missed_deadlines = false;
+volatile bool trigger_exec = true;
+
+// PID Variables
+volatile double kpx = 0.3; // Proportional sControl Constant
+volatile int kdx = 80; // Derivative Control Constant
+volatile double kpy = 0.2; // Proportional Control Constant
+volatile int kdy = 60; // Derivative Control Constant
+
+
+float t = 0.01;
+
+uint16_t x_current_pos = 0;
+uint16_t y_current_pos = 0;
+uint16_t x_current_filtered = 0;
+uint16_t x_previous_pos = 0;
+uint16_t x_previous_filtered = 0;
+
+uint16_t y_current_filtered = 0;
+uint16_t y_previous_pos = 0;
+uint16_t y_previous_filtered = 0;
+uint16_t x_set_pos = 0;
+uint16_t y_set_pos = 0;
+
+int error_x = 0;
+int derivative_x = 0;
+int last_error_x = 0;
+int error_y = 0;
+int derivative_y = 0;
+int last_error_y = 0;
+int theta_x = 0;
+int theta_y = 0;
+double dutycicle_x = .0;
+double dutycicle_y = .0;
+
+
 int deadlines_missed = 0;
 
 /*
@@ -92,20 +120,16 @@ void initialize_timer() {
     SETBIT(T1CONbits.TON); // Start Timer  
 
 
-    //    //setup Timer 2
-    //    CLEARBIT(T2CONbits.TON); // Disable Timer
-    //    CLEARBIT(T2CONbits.TCS); // Select internal instruction cycle clock
-    //    CLEARBIT(T2CONbits.TGATE); // Disable Gated Timer mode
-    //    TMR2 = 0x00; // Clear timer register
-    //    T2CONbits.TCKPS = 0b10; // Select 1:64 Prescaler
-    //    CLEARBIT(IFS0bits.T2IF); // Clear Timer2 interrupt status flag
-    //    CLEARBIT(IEC0bits.T2IE); // Disable Timer2 interrupt enable control bit
-    //    PR2 = 4000; // Set timer period 20ms: // 4000= 20*10^-3 * 12.8*10^6 * 1/64
-    //    SETBIT(T2CONbits.TON); /* Turn Timer 2 on */
-
-
-
-
+    //setup Timer 2
+    CLEARBIT(T2CONbits.TON); // Disable Timer
+    CLEARBIT(T2CONbits.TCS); // Select internal instruction cycle clock
+    CLEARBIT(T2CONbits.TGATE); // Disable Gated Timer mode
+    TMR2 = 0x00; // Clear timer register
+    T2CONbits.TCKPS = 0b10; // Select 1:64 Prescaler
+    CLEARBIT(IFS0bits.T2IF); // Clear Timer2 interrupt status flag
+    CLEARBIT(IEC0bits.T2IE); // Disable Timer2 interrupt enable control bit
+    PR2 = 4000; // Set timer period 20ms: // 4000= 20*10^-3 * 12.8*10^6 * 1/64
+    SETBIT(T2CONbits.TON); /* Turn Timer 2 on */
 
 }
 
@@ -257,53 +281,143 @@ uint16_t touch_screen_position_read_y(void) {
 
 }
 
+void read_touchscreen() {
+
+    //Read the touchscreen
+    touch_screen_dimension_set(x);
+    x_current_pos = touch_screen_position_read_x();
+    touch_screen_dimension_set(y);
+    y_current_pos = touch_screen_position_read_y();
+
+
+}
+
+void setPointGenerator() {
+
+    x_set_pos = center_x + radius * sin(angular_velocity_w * t);
+    y_set_pos = center_y + radius * cos(angular_velocity_w * t);
+    t = t + 0.01;
+    // returns set point (X,Y) as they are located on circle
+}
+
+/*
+ * Butterworth Filter N=1, Cutoff 3 Hz, sampling @ 50 Hz
+ *
+ * Butterworth formula from Matlab
+ * [b,a]=butter(N,Wn,ftype)
+ * N: Nth-order
+ * Wn: Normalized cutoff frequency
+ * ftype: high or low
+ * Wn = cufOffFrequency/(0.5*samplingRate)      Nyquist Theorem
+ * 
+ * N=1
+ * Wn=3/(0.5*50)=3/25=0.12
+ * Using [b,a]=butter(1, 0.12, 'low') we got:
+ * b =
+ *      0.1602  0.1602
+ * a = 
+ *      1.0000  -0.6796
+ */
+
 uint16_t butterworth_filter_x(x) {
-    x = filter(x);
-    return x;
+    x_current_filtered = (0.1602 * x) + (0.1602 * x_previous_pos) + (0.6796 * x_previous_filtered);
+    x_previous_pos = x;
+    x_previous_filtered = x_current_filtered;
+    return x_current_filtered;
 }
 
 uint16_t butterworth_filter_y(y) {
-    y = filter(y);
-    return y;
-}
-
-void read_touchscreen() {
-    
-    //Read the touchscreen
-    touch_screen_dimension_set(x);
-    x_pos = touch_screen_position_read_x();
-    touch_screen_dimension_set(y);
-    y_pos = touch_screen_position_read_y();
-
-
+    y_current_filtered = (0.1602 * y) + (0.1602 * y_previous_pos) + (0.6796 * y_previous_filtered);
+    y_previous_pos = y;
+    y_previous_filtered = y_current_filtered;
+    return y_current_filtered;
 }
 
 /*
  * PD Controller
  */
-void pd_controller(x_pos, y_pos) {
-    // filter x coordinate
-    x_pos = butterworth_filter_x(x_pos);
-    y_pos = butterworth_filter_y(y_pos);
-    
+void pd_controller(x_current_pos, y_current_pos) {
+    setPointGenerator(); // generate setpoint x and y
+
+    //        lcd_locate(0, 3);
+    //        lcd_printf("x set pos: %u", x_set_pos);
+    //        lcd_locate(0, 4);
+    //        lcd_printf("y set pos: %u", y_set_pos);
+
+    // filter x coordinate and y coordinate
+    x_current_pos = butterworth_filter_x(x_current_pos);
+    // calculate the error
+    error_x = x_set_pos - x_current_pos;
+    // calculate the derivative
+    derivative_x = error_x - last_error_x;
+
+    // calculate the control variable, period of the loop is 20ms
+    theta_x = (kpx * error_x)+(kdx * (derivative_x / 20));
+
+    // Saturate (map) the pwm control variable to protect motor
+
+
+    last_error_x = error_x;
+
+    y_current_pos = butterworth_filter_y(y_current_pos);
+    //    lcd_locate(0, 3);
+    //    lcd_printf("x_f_pos: %u", x_current_pos);
+    //    lcd_locate(0, 4);
+    //    lcd_printf("y_f_pos: %u", y_current_pos);
+
+
+    // calculate the error
+    error_y = y_set_pos - y_current_pos;
+    // calculate the derivative
+    derivative_y = error_y - last_error_y;
+    // calculate the control variable, period of the loop is 20ms
+    theta_y = (kpy * error_y)+(kdy * derivative_y / 20);
+
+    // Saturate (map) the pwm control variable to protect motor
+
+
+    last_error_y = error_y;
+
+
+
+    if (theta_x<-300) {
+        theta_x = -300;
+    } else if (theta_x > 300) {
+        theta_x = 300;
+    }
+    if (theta_y<-300) {
+        theta_y = -300;
+    } else if (theta_y > 300) {
+        theta_y = 300;
+    }
+
+    //        lcd_locate(0, 3);
+    //        lcd_printf("                         ");
+    //        lcd_locate(0, 3);
+    //        lcd_printf("thetaX: %d", theta_x);
+    //        lcd_locate(0, 4);
+    //        lcd_printf("                         ");
+    //        lcd_locate(0, 4);
+    //        lcd_printf("thetaY: %d", theta_y);
+
+    // transfer the range from -400-400 to 0.9-2.1
+    dutycicle_x = (theta_x - (-300)) / 600.0 * (2.1 - 0.9) + 0.9;
+    dutycicle_y = (theta_y - (-300)) / 600.0 * (2.1 - 0.9) + 0.9;
+    //        lcd_locate(0, 3);
+    //        lcd_printf("                         ");
+    //        lcd_locate(0, 3);
+    //        lcd_printf("thetaX: %f", dutycicle_x);
+    //        lcd_locate(0, 4);
+    //        lcd_printf("                         ");
+    //        lcd_locate(0, 4);
+    //        lcd_printf("thetaY: %f", dutycicle_y);
 }
-
-/*
- * Butterworth Filter N=1, Cutoff 3 Hz, sampling @ 50 Hz
- */
-
-
-
-
-
-
-
 
 void print_missed_deadlines() {
     lcd_locate(0, 7);
     lcd_printf("missed deadlines: %u", deadlines_missed);
 
-   
+
 }
 
 /*
@@ -320,20 +434,29 @@ void main_loop() {
 
     touch_screen_init();
 
+    // initialize servos
+    servo_init(SERVO_1);
+    Nop();
+    Nop();
+    servo_init(SERVO_2);
+    Nop();
+    Nop();
+
     while (TRUE) {
 
-        //        servo_control(20 - 0.9, 20 - 2.1);
-        //        if (trigger_exec == true) {
         if (trigger_read_touchscreen == true) {
             read_touchscreen();
             trigger_read_touchscreen = false;
 
         }
 
+
+
         if (trigger_pd_controller == true) {
-            pd_controller();
+            pd_controller(x_current_pos, y_current_pos);
             trigger_pd_controller = false;
         }
+        servo_control(20 - dutycicle_x, 20 - dutycicle_y);
 
         if (trigger_print_missed_deadlines == true) {
             print_missed_deadlines();
@@ -341,14 +464,6 @@ void main_loop() {
         }
 
 
-
-        //            trigger_exec = false;
-        //        }
-
-        lcd_locate(0, 5);
-        lcd_printf("x pos: %u", x_pos);
-        lcd_locate(0, 6);
-        lcd_printf("y pos: %u", y_pos);
 
 
     }
@@ -364,7 +479,13 @@ void __attribute__((__interrupt__, __shadow__, __auto_psv__)) _T1Interrupt(void)
     counterHundredHz++;
     if (counterHundredHz >= 1) {
         counterHundredHz = 0;
-        trigger_read_touchscreen = true;
+
+        if (trigger_read_touchscreen == true) {
+            deadlines_missed++;
+        } else {
+            trigger_read_touchscreen = true;
+
+        }
 
     }
 
@@ -372,7 +493,12 @@ void __attribute__((__interrupt__, __shadow__, __auto_psv__)) _T1Interrupt(void)
     counterFiftyHz++;
     if (counterFiftyHz >= 2) {
         counterFiftyHz = 0;
-        trigger_pd_controller = true;
+        if (trigger_pd_controller == true) {
+            deadlines_missed++;
+        } else {
+            trigger_pd_controller = true;
+
+        }
     }
 
     // 5Hz for Task3 , 200 ms
@@ -382,23 +508,6 @@ void __attribute__((__interrupt__, __shadow__, __auto_psv__)) _T1Interrupt(void)
         trigger_print_missed_deadlines = true;
     }
 
-    //    if (trigger_exec == true) {
-    //        // deadline missed
-    //        deadlines_missed++;
-    //    }
-    //
-    //    trigger_exec = true;
-
-
-
-
-
 
 }
 
-
-// only one timer should be used
-
-// don't forget to add saturation
-
-// deadline 100Hz, display of deadline 5Hz
